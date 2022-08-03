c90a9420cb8b786a6d5de132f728248140fbf215
Margarita Khokhlova <margarita.khokhlova@fujitsu.com>
2022-08-03 23:45:24 +0200

    add split train val functionality

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ if __name__ == "__main__":
     print(data.Y)
     print(len(data.X))
     print(len(data.X[0]))
+    # check splitting function
+    #X_train, y_train, X_test, y_test = data.get_train_val_split() 
+    data.get_train_val_split()
+    print(f'There are {len(data.y_train)} train sequences for labels {data.y_train} and {len(data.y_test)} sequences with labels {data.y_test}')
--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,19 +1,38 @@
 import os
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
 
 class PatientsRawData:
     def __init__(self, file_path):        
         self.path = file_path
         self.X = []
         self.Y = []
+        self.X_train = None
+        self.y_train = None
+        self.X_test = None
+        self.y_test = None
+
     def load_data(self):
         subfolders = [ f.path for f in os.scandir(self.path) if f.is_dir()]
         for s_folder in subfolders:
             for filename in os.listdir(s_folder):
                 f = os.path.join(s_folder, filename)
                 with open(f) as datafile:
-                    contents = datafile.read()
-                    self.X.append(contents)
+                    lst = [int(x) for x in datafile.read().rstrip().split()]
+                    self.X.append(lst)
                     self.Y.append(s_folder.split('_')[-1])
+
+    def shuffle_data(self, random_state=0):
+        self.X, self.Y = shuffle(self.X, self.Y, random_state)
+
+    def get_train_val_split(self, test_size = 0.2, stratified = True):
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.Y,
+                                                    stratify=self.Y, test_size=test_size)
+
+
+        
+
+
 
 
 
